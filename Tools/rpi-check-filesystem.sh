6681c785ae67a80b263d6ff9978b055e09c3e313
ubuntu <ubuntu@ubuntu>
2021-01-05 22:20:14 +0100

    Set of apps that run in the terminal
    Apps in the terminal
    apt add key
    
    search swapfile starts with

---
+++ Tools/rpi-check-filesystem.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ `whoami` != 'root' ]; then
+    echo "You must be root to run this script"
+    read -n1 -p "Press any key to exit..."
+    exit
+fi
+
+echo "Starting filesystem check!"
+
+SWAP_CONFIG_FILE="/etc/dphys-swapfile"
+SWAP_FILE_PATH=$( cat /etc/dphys-swapfile | grep ^CONF_SWAPFILE | awk '{print $1}' | sed -r 's/^CONF_SWAPFILE=([\/a-zA-Z0-9]*)$/\1/' )
+echo "Swapfile is at $SWAP_FILE_PATH"
+
+DOCKER_MOUNTPOINT="/var/lib/docker"
+DOCKER_UUID=$( cat /etc/fstab | grep $DOCKER_MOUNTPOINT | awk '{print $1}' | sed -r 's/^UUID=([0-9a-fA-F-]{36})$/\1/' )
+echo "Docker volumes are mounted at $DOCKER_MOUNTPOINT, the drive has UUID $DOCKER_UUID"
+
+echo "Stopping btcpayserver..."
+service btcpayserver stop
+
+echo "Stopping docker..."
+service docker stop
+
+echo "Turning off swap..."
+dphys-swapfile swapoff
+
+echo "Removing swap file..."
+rm $SWAP_FILE_PATH
+
+echo "Unmounting $DOCKER_MOUNTPOINT..."
+umount $DOCKER_MOUNTPOINT
+
+echo "Checking filesystem at /dev/disk/by-uuid/$DOCKER_UUID..."
+fsck -f /dev/disk/by-uuid/$DOCKER_UUID
+
+echo "Mounting $DOCKER_MOUNTPOINT..."
+mount $DOCKER_MOUNTPOINT
+
+echo "Starting docker..."
+service docker start
+
+echo "Starting btcpayserver..."
+service btcpayserver start
+
+echo "Setting up swap..."
+dphys-swapfile setup
+
+echo "Turning swap on..."
+dphys-swapfile swapon
+
+read -n1 -p "All done! Press any key to exit..."
+exit
